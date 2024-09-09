d6fb4e7156403a28599fee279d7d5ab9260578bb
Amaan ALvi <amaanalvi40@gmail.com>
2024-09-09 11:38:09 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import Hero from './section/Hero/Hero';
+import Projects from './section/Projects/Projects';
+import Skills from './section/Skills/Skills';
+import Contact from './section/Contact/Contact';
+import Footer from './section/Footer/Footer';
+function App() {
+  return (
+    
+    <>
+      <Hero/>
+      <Projects/>
+      <Skills/>
+      <Contact/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
